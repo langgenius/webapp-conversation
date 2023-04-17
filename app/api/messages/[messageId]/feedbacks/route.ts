87286f9dadcab2499f3b28009fde462ef317cfcf
Joel <iamjoel007@gmail.com>
2023-04-17 13:58:58 +0800

    feat: support feedback api

---
+++ app/api/messages/[messageId]/feedbacks/route.ts
@@ -0,0 +1,16 @@
+import { type NextRequest } from 'next/server'
+import { NextResponse } from 'next/server'
+import { getInfo, client } from '@/app/api/utils/common'
+
+export async function POST(request: NextRequest, { params }: {
+  params: { messageId: string }
+}) {
+  const body = await request.json()
+  const {
+    rating
+  } = body
+  const { messageId } = params
+  const { user } = getInfo(request);
+  const { data } = await client.messageFeedback(messageId, rating, user)
+  return NextResponse.json(data)
+}
