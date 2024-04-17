eee0b4af910c4e66aab1800d806df7c19f7c185d
MMHK <sam@mixmedia.com>
2024-04-17 20:28:04 +0800

    完善代理兼容

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -43,16 +43,26 @@ export async function GET(request: NextRequest) {
   const data = await getSessionFromRequest(request);
   const {sessionId} = getInfo(request);
 
+  const hosts = request.headers.get('x-forwarded-host')?.split(',') || [];
+  const ports = request.headers.get('x-forwarded-port')?.split(',') || [];
+  const protos = request.headers.get('x-forwarded-proto')?.split(',') || [];
+
+  const url = request.nextUrl.clone()
+  url.host = hosts[0] || request.nextUrl.host
+  url.port = ports[0] || request.nextUrl.port
+  url.protocol = protos[0] || request.nextUrl.protocol
+  url.pathname = '/';
+  url.search = '';
+
   if (data) {
-    console.log(data);
-    const url = request.nextUrl.clone()
-    url.pathname = '/';
-    url.search = '';
+    // console.log(data);
     return ResponseWithSession(NextResponse.redirect(url), sessionId, data);
   }
 
   return NextResponse.json({
-    status: false,
-    error: 'auth error',
+    headers: request.headers,
+    url,
   })
+
+  // return NextResponse.redirect(url);
 }
--- middleware.ts
+++ middleware.ts
@@ -24,6 +24,7 @@ export async function middleware(request: NextRequest) {
 
         return NextResponse.next({
             headers: setSession(sessionId),
+            request,
         });
     }
 
