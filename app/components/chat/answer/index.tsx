6933b5923b5bb27e54682f94447a8e86501a5a50
JzoNg <jzongcode@gmail.com>
2024-04-23 18:23:06 +0800

    fix style of answer

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -177,7 +177,7 @@ const Answer: FC<IAnswerProps> = ({
         </div>
         <div className={`${s.answerWrap}`}>
           <div className={`${s.answer} relative text-sm text-gray-900`}>
-            <div className={'ml-2 py-3 px-4 bg-gray-100 rounded-tr-2xl rounded-b-2xl'}>
+            <div className={`ml-2 py-3 px-4 bg-gray-100 rounded-tr-2xl rounded-b-2xl ${workflowProcess && 'min-w-[480px]'}`}>
               {workflowProcess && (
                 <WorkflowProcess data={workflowProcess} hideInfo />
               )}
