0b7c900444913e468d9f8301858f8c8b8d075dab
kaledfwich <81383160+kaledfwich@users.noreply.github.com>
2024-08-01 20:29:40 -0300

    Update index.tsx

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -332,7 +332,7 @@ const Welcome: FC<IWelcomeProps> = ({
               : <div>
               </div>}
             <a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
-              <span className='uppercase'>{t('app.chat.powerBy')}</span>
+              <span className='uppercase'>"Teste"</span>
               <FootLogo />
             </a>
           </div>
