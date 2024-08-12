7f24387eef8b3cd7c2a1c78b46eeb89415fbf819
Yusuke Tenkyo <z.yusuke.tenkyo@gree.net>
2024-08-12 18:44:49 +0900

    Add Japanese language settings to i18n”

---
+++ i18n/lang/common.ja.ts
@@ -0,0 +1,33 @@
+const translation = {
+  api: {
+    success: '成功',
+    saved: '保存しました',
+    create: '作成しました',
+  },
+  operation: {
+    confirm: '確認',
+    cancel: 'キャンセル',
+    clear: 'クリア',
+    save: '保存',
+    edit: '編集',
+    refresh: '再起動',
+    search: '検索',
+    send: '送信',
+    lineBreak: '改行',
+    like: 'いいね',
+    dislike: 'よくないね',
+    ok: 'OK',
+  },
+  imageUploader: {
+    uploadFromComputer: 'コンピューターからアップロード',
+    uploadFromComputerReadError: '画像の読み込みに失敗しました。もう一度お試しください。',
+    uploadFromComputerUploadError: '画像のアップロードに失敗しました。もう一度アップロードしてください。',
+    uploadFromComputerLimit: 'アップロードする画像は{{size}} MBを超えてはいけません',
+    pasteImageLink: '画像リンクを貼り付け',
+    pasteImageLinkInputPlaceholder: 'ここに画像リンクを貼り付けてください',
+    pasteImageLinkInvalid: '無効な画像リンクです',
+    imageUpload: '画像アップロード',
+  },
+}
+
+export default translation
