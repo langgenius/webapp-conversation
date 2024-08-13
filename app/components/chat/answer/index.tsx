24da5217da3ac943e1e877cd96a96cfcfb2dc510
dream-rec <92309085+dream-rec@users.noreply.github.com>
2024-08-13 11:51:45 +0800

    Update index.tsx

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -178,9 +178,9 @@ const Answer: FC<IAnswerProps> = ({
         <div className={`${s.answerWrap}`}>
           <div className={`${s.answer} relative text-sm text-gray-900`}>
             <div className={`ml-2 py-3 px-4 bg-gray-100 rounded-tr-2xl rounded-b-2xl ${workflowProcess && 'min-w-[480px]'}`}>
-              {workflowProcess && (
+              {/* {workflowProcess && (
                 <WorkflowProcess data={workflowProcess} hideInfo />
-              )}
+              )} */}
               {(isResponding && (isAgentMode ? (!content && (agent_thoughts || []).filter(item => !!item.thought || !!item.tool).length === 0) : !content))
                 ? (
                   <div className='flex items-center justify-center w-6 h-5'>
