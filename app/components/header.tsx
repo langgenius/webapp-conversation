120dc31d3b8a5868df4c87fd9f51cb799a0520dd
Slarrauri <slarrauri@gmail.com>
2025-01-06 15:36:53 -0300

    Change: remove icon from header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -4,7 +4,7 @@ import {
   Bars3Icon,
   PencilSquareIcon,
 } from '@heroicons/react/24/solid'
-import AppIcon from '@/app/components/base/app-icon'
+// import AppIcon from '@/app/components/base/app-icon'
 export type IHeaderProps = {
   title: string
   isMobile?: boolean
@@ -30,7 +30,17 @@ const Header: FC<IHeaderProps> = ({
         )
         : <div></div>}
       <div className='flex items-center space-x-2'>
-        <AppIcon size="small" />
+        {/* <AppIcon size="small" /> */}
+        <img
+          src="https://control.gpt.ar/assets/41e5931c-660f-4e56-8ef0-0956eef31d08?key=system-small-cover"
+          alt="example"
+          style={{
+            borderRadius: '100%',
+            width: '42px', // Ajusta el tamaño según tus necesidades
+            height: '42px', // Asegúrate de que sea igual para un círculo perfecto
+            // objectFit: 'cover', // Mantiene el contenido ajustado
+          }}
+        />
         <div className=" text-sm text-gray-800 font-bold">{title}</div>
       </div>
       {isMobile
