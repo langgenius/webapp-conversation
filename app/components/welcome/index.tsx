ca258d04b85d1bef2afa609c6decdd2ef383acf7
Slarrauri <slarrauri@gmail.com>
2025-01-06 15:38:35 -0300

    Change: brand

--- app/components/welcome/index.tsx
+++ app/components/welcome/index.tsx
@@ -340,7 +340,7 @@ const Welcome: FC<IWelcomeProps> = ({
               </div>
               : <div>
               </div>}
-            <a className='flex items-center pr-3 space-x-3' href="https://dify.ai/" target="_blank">
+            <a className='flex items-center pr-3 space-x-3' href="https://gpt.ar/" target="_blank">
               <span className='uppercase'>{t('app.chat.powerBy')}</span>
               <FootLogo />
             </a>
