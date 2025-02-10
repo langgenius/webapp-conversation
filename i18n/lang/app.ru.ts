db12f648deb6c06665f4192108a75bc57d6290a1
Aleks <seoinetru@gmail.com>
2025-02-10 10:22:02 +0300

    Добавил русский перевод

---
+++ i18n/lang/app.ru.ts
@@ -0,0 +1,31 @@
+const translation = {
+    common: {
+        welcome: 'Добро пожаловать',
+        appUnavailable: 'Приложение недоступно',
+        appUnkonwError: 'Приложение недоступно',
+    },
+    chat: {
+        newChat: 'Новый чат',
+        newChatDefaultName: 'Новая беседа',
+        openingStatementTitle: 'Вступительное слово',
+        powerBy: 'При поддержке',
+        prompt: 'Подсказка',
+        privatePromptConfigTitle: 'Настройки беседы',
+        publicPromptConfigTitle: 'Начальная подсказка',
+        configStatusDes: 'Перед началом вы можете изменить настройки беседы',
+        configDisabled: 'Для этой сессии использованы настройки предыдущей сессии.',
+        startChat: 'Начать чат',
+        privacyPolicyLeft: 'Пожалуйста, прочтите ',
+        privacyPolicyMiddle: 'политику конфиденциальности',
+        privacyPolicyRight: ', предоставленную разработчиком приложения.',
+    },
+    errorMessage: {
+        valueOfVarRequired: 'Значения переменных не могут быть пустыми',
+        waitForResponse: 'Пожалуйста, дождитесь завершения ответа на предыдущее сообщение.',
+    },
+    variableTable: {
+        optional: 'Необязательно',
+    },
+}
+
+export default translation
---
+++ i18n/lang/common.ru.ts
@@ -0,0 +1,33 @@
+const translation = {
+    api: {
+        success: 'Успешно',
+        saved: 'Сохранено',
+        create: 'Создано',
+    },
+    operation: {
+        confirm: 'Подтвердить',
+        cancel: 'Отмена',
+        clear: 'Очистить',
+        save: 'Сохранить',
+        edit: 'Редактировать',
+        refresh: 'Перезапустить',
+        search: 'Поиск',
+        send: 'Отправить',
+        lineBreak: 'Перенос строки',
+        like: 'нравится',
+        dislike: 'не нравится',
+        ok: 'OK',
+    },
+    imageUploader: {
+        uploadFromComputer: 'Загрузить с компьютера',
+        uploadFromComputerReadError: 'Ошибка чтения изображения, пожалуйста, попробуйте снова.',
+        uploadFromComputerUploadError: 'Ошибка загрузки изображения, пожалуйста, загрузите снова.',
+        uploadFromComputerLimit: 'Загружаемые изображения не могут превышать {{size}} МБ',
+        pasteImageLink: 'Вставить ссылку на изображение',
+        pasteImageLinkInputPlaceholder: 'Вставьте ссылку на изображение здесь',
+        pasteImageLinkInvalid: 'Недействительная ссылка на изображение',
+        imageUpload: 'Загрузка изображения',
+    },
+}
+
+export default translation
