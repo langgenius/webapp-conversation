972ee5e5abdaaf76ef2990ce7dddb3180990ffd4
behappy216 <66057724+behappy216@users.noreply.github.com>
2025-02-14 21:14:42 +0800

    Update index.tsx

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -51,7 +51,7 @@ const Main: FC<IMainProps> = () => {
 
   useEffect(() => {
     if (APP_INFO?.title)
-      document.title = `${APP_INFO.title} - Powered by Dify`
+      document.title = `${APP_INFO.title}`
   }, [APP_INFO?.title])
 
   // onData change thought (the produce obj). https://github.com/immerjs/immer/issues/576
