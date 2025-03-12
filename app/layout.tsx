2befe98e8e4d2b835ae98f28297c7be3bfacfb6e
Deva <66057724+BeHappy216@users.noreply.github.com>
2025-03-12 20:09:21 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,7 @@ const LocaleLayout = ({
   const locale = getLocaleOnServer()
   return (
     <html lang={locale ?? 'en'} className="h-full">
-      <body className="h-full">
-        <body className="h-full font-sans"> {/* 关键修改：添加 font-sans class */}
+      <body className="h-full font-sans"> {/* 关键修改：添加 font-sans class */}
         <div className="overflow-x-auto">
           <div className="w-screen h-screen min-w-[300px]">
             {children}
