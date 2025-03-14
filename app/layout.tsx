87406d82fe3807a4bd2404013b7425a6b47b369a
Deva <66057724+BeHappy216@users.noreply.github.com>
2025-03-14 14:46:26 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,13 @@ const LocaleLayout = ({
   const locale = getLocaleOnServer()
   return (
     <html lang={locale ?? 'en'} className="h-full">
+      <head>
+        {/* 引入 CDN Web 字体 */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/@chinese-fonts/lxgwwenkaibright@1.0.2/dist/LXGWBright-Regular/result.css"
+          rel="stylesheet"
+        />
+      </head>
       <body className="h-full">
         <div className="overflow-x-auto">
           <div className="w-screen h-screen min-w-[300px]">
